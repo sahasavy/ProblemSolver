ed77c9ce69a547e6187ebddd3855d56d95307901
Sourav Saha <sahasavy@gmail.com>
2024-04-13 16:09:00 +0530

    feat: BRN-01 - Initial commits (#1)

---
+++ src/main/java/pvt/code/practice/easy/Easy1.java
@@ -0,0 +1,8 @@
+package pvt.code.practice.easy;
+
+public class Easy1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/pvt/code/practice/ProblemSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package pvt.code.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
