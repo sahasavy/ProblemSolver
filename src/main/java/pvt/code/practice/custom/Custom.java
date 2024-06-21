26132b7a0edb44f2fe103eefb3322407c91a44fb
Sourav Saha <sahasavy@gmail.com>
2024-06-21 22:07:22 +0530

    feat: BRN-02 - Few more problems

---
+++ src/main/java/pvt/code/practice/custom/Custom.java
@@ -0,0 +1,10 @@
+package pvt.code.practice.custom;
+
+/*
+
+ */
+public class Custom {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/pvt/code/practice/scenarios/RateLimiter.java
@@ -0,0 +1,45 @@
+package pvt.code.practice.scenarios;
+
+import java.time.Instant;
+
+public class RateLimiter {
+    private final int maxRequests;
+    private final long refillIntervalMillis;
+    private int tokens;
+    private long lastRefillTime;
+
+    public RateLimiter(int maxRequests, long refillIntervalMillis) {
+        this.maxRequests = maxRequests;
+        this.refillIntervalMillis = refillIntervalMillis;
+        this.tokens = maxRequests;
+        this.lastRefillTime = Instant.now().toEpochMilli();
+    }
+
+    public synchronized boolean allowRequest(int reqId) {
+        refillTokens(reqId);
+
+        if (tokens > 0) {
+            tokens--;
+            System.out.println("Request " + reqId + " || Subtracting Tokens from bucket || UPDATED Value of tokens : " + tokens);
+            return true;
+        }
+        return false;
+    }
+
+    private void refillTokens(int reqId) {
+        long now = Instant.now().toEpochMilli();
+        long elapsedTime = now - lastRefillTime;
+        int tokensToAdd = (int) (elapsedTime / refillIntervalMillis);
+
+        System.out.println("Request " + reqId + " || Value of now : " + now);
+        System.out.println("Request " + reqId + " || Value of elapsedTime : " + elapsedTime);
+        System.out.println("Request " + reqId + " || Value of tokensToAdd : " + tokensToAdd);
+
+        if (tokensToAdd > 0) {
+            tokens = Math.min(tokens + tokensToAdd, maxRequests);
+            lastRefillTime = now;
+            System.out.println("Request " + reqId + " || Adding Tokens to bucket || UPDATED Value of tokens : " + tokens);
+            System.out.println("Request " + reqId + " || UPDATED Value of lastRefillTime : " + lastRefillTime);
+        }
+    }
+}
